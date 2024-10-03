96ba1c2e7c1430eaee97e35e2f766cc6ab9a799e
Sergio González Velasco <124403735+Sergonve1@users.noreply.github.com>
2024-10-03 08:49:54 +0200

    Setup project

---
+++ GestionadorComunidades/src/main/java/TFG/GestionadorComunidades/GestionadorComunidadesApplication.java
@@ -0,0 +1,13 @@
+package TFG.GestionadorComunidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionadorComunidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionadorComunidadesApplication.class, args);
+	}
+
+}
---
+++ GestionadorComunidades/src/test/java/TFG/GestionadorComunidades/GestionadorComunidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package TFG.GestionadorComunidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionadorComunidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
