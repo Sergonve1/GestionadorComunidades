8dbb2f10c4624d52eca19dda53ea93b51e724747
Sergio Gonzalez Velasco <sergio.gonve95@gmial.com>
2024-10-03 13:26:36 +0200

    Organización del proyecto

--- GestionadorComunidades/src/main/java/TFG/GestionadorComunidades/FrameworksDrivers/Application/GestionadorComunidadesApplication.java
+++ GestionadorComunidades/src/main/java/TFG/GestionadorComunidades/FrameworksDrivers/Application/GestionadorComunidadesApplication.java
@@ -1,4 +1,4 @@
-package TFG.GestionadorComunidades;
+package TFG.GestionadorComunidades.FrameworksDrivers.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
